971ba800d499f07abf1f2b23530602dc77eb4691
ArtemCH240322 <sun2383@mail.ru>
2022-08-05 11:57:04 +0300

    Первый комит. Создал ветки. Код CC. Код CS. Код DBZE

--- src/main/java/com/example/hw2_12/exception/DivideByZeroException.java
+++ src/main/java/com/example/hw2_12/exception/DivideByZeroException.java
@@ -1,4 +1,6 @@
 package com.example.hw2_12.exception;
 
-public class DivideByZeroException {
+public class DivideByZeroException extends IllegalArgumentException {
+
+    public DivideByZeroException(String message) { super(message);}
 }
